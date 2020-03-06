4e63d9a13bad2c4096df22db161d15387601854e
natalia <zhestckova.natali@yandex.ru>
2020-03-06 03:40:30 +0300

    update register form

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,13 +1,10 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-info",
   templateUrl: "./info.component.html",
   styleUrls: ["./info.component.less"]
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent {
   @Input() info: string;
-  ngOnInit(): void {
-    this.info = `Придумайте логин, используя только латинские буквы, символы подчеркивания и цифры`;
-  }
 }
